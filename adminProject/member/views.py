02065702de13cb36d06d9ef61bfbb020e7016829
Guarz1a <dotranquanganh123@gmail.com>
2024-09-26 15:09:28 +0700

    Anh

--- adminProject/member/views.py
+++ adminProject/member/views.py
@@ -3,6 +3,10 @@ from django.template import loader
 from .forms import EmployeeForm, ProductForm
 from django.shortcuts import render, redirect,get_object_or_404
 from .models import Employee, Product
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+
 
 
 
@@ -14,10 +18,8 @@ def loadNhanSu(request):
             return redirect('nhanSu')
     else: 
         form = EmployeeForm()
-
     # Query the database for all employees
     employees = Employee.objects.all()
-
     return render(request, 'nhanSu.html', {'form': form, 'employees': employees})
 
 
@@ -30,8 +32,7 @@ def updateNhanSu(request, employee_id):
             form.save()
             return redirect('nhanSu')  # Redirect về trang danh sách nhân viên
     else:
-        form = EmployeeForm(instance=employee)
-    
+        form = EmployeeForm(instance=employee)    
     return render(request, 'nhanSu.html', {'form': form})
 
 
@@ -40,8 +41,7 @@ def deleteNhanSu(request, employee_id):
     employee = get_object_or_404(Employee, employee_id=employee_id)  # Kiểm tra nhân viên có tồn tại không
     if request.method == 'POST':
         employee.delete()  # Xóa nhân viên
-        return redirect('nhanSu')  # Redirect về trang danh sách nhân viên
-    
+        return redirect('nhanSu')  # Redirect về trang danh sách nhân viên   
     return render(request, 'nhanSu.html', {'employee': employee})
 
 
@@ -53,7 +53,6 @@ def loadProduct(request):
             return redirect('product')
     else: 
         form = ProductForm()
-
     # Query the database for all employees
     product = Product.objects.all()
 
@@ -68,8 +67,7 @@ def updateProduct(request, product_id):
             form.save()
             return redirect('product')  # Redirect về trang danh sách nhân viên
     else:
-        form = ProductForm(instance=product)
-    
+        form = ProductForm(instance=product)   
     return render(request, 'Products.html', {'form': form})
 
 
@@ -79,8 +77,7 @@ def deleteProduct(request, product_id):
     product = get_object_or_404(Product, product_id=product_id)  # Kiểm tra nhân viên có tồn tại không
     if request.method == 'POST':
         product.delete()  # Xóa nhân viên
-        return redirect('product')  # Redirect về trang danh sách nhân viên
-    
+        return redirect('product')  # Redirect về trang danh sách nhân viên  
     return render(request, 'product.html', {'Products': product})
 
 def loadShopProduct(request):
@@ -91,4 +88,13 @@ def loadShopProduct(request):
     return render(request, 'Shop.html', {'Products': product})
 
 def loadLogin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.error(request, 'login tc.')
+        else:
+            messages.error(request, 'Tên đăng nhập hoặc mật khẩu không chính xác.')
     return render(request, 'login.html')
